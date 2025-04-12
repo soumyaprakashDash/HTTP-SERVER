d49ea0a487b0ca7e3a1696857d0b993b0e7a1adf
Sonu <soumyaprakashdash23@gmail.com>
2025-04-12 10:38:25 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+// This is use query parameter
+const express = require ('express');
+
+const app = express ();
+
+function sum(n){
+    let ans = 0;
+    for (let i=1 ; i<=n ;i++){
+        ans =ans +i;
+    }
+    return ans ;
+
+
+}
+app.get ('/',function (req,res){
+    const n =req.query.n;
+    const ans = sum(n);
+    res.send ("hi your ans is "+ ans);
+})
+app.listen (3000);
